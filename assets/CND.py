9cca006fecc510a6822f5e70d4a27951ee244e54
HN <88585967+ImNoobb@users.noreply.github.com>
2021-12-06 17:52:57 +0700

    Add files via upload

--- assets/CND.py
+++ assets/CND.py
@@ -25,4 +25,23 @@ class A_Button():
 
 		scr.blit(self.button_img[int(self.index)],self.rect)
 
+class N_Button():
+	def __init__(self,btn_img,btn_img_hover,pos,cmd):
+		self.button = [btn_img,btn_img_hover]
+		self.rect = self.button[0].get_rect(topleft=pos)
+		self.cmd = cmd
+		self.index = 0
+
+	def run(self,scr,click,s_m_o_f):
+		mouse_pos = pygame.mouse.get_pos()
+		if self.rect.collidepoint(mouse_pos):
+			self.index = 1
+			if click and pygame.mouse.get_pressed()[0]:
+				self.cmd()
+				s_m_o_f()
+		else:
+			self.index = 0
+
+		scr.blit(self.button[self.index],self.rect)
+
 		
