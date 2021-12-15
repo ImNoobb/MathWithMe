a45d941845cc7ddf22962197298c837836a9acc9
HN <88585967+ImNoobb@users.noreply.github.com>
2021-12-15 16:53:02 +0700

    Add files via upload

--- assets/lop_import.py
+++ assets/lop_import.py
@@ -1,13 +1,27 @@
 import pygame
 from pygame.locals import *
 
-lop6_dai_I = []
+lop6_baigiang = []
 list_temp = []
-for i in range(7):
-    tempppp = pygame.image.load(f'assets\\baigiang\\lop6\\bai{i+1}.png')
+for i in range(18):
+    tempppp = pygame.image.load(f'assets\\baigiang\\lop6\\bai{i+1}.png').convert()
     list_temp.append(tempppp)
-lop6_dai_I.append(list_temp)
+lop6_baigiang.append(list_temp)
 
-lop6_dai_II = []
-for i in range(1):
-    pass
+list_temp = []
+for i in range(13):
+    tempppp = pygame.image.load(f'assets\\baigiang\\lop6\\bai{i+1}_1.png').convert()
+    list_temp.append(tempppp)
+lop6_baigiang.append(list_temp)
+
+list_temp = []
+for i in range(17):
+    tempppp = pygame.image.load(f'assets\\baigiang\\lop6\\bai{i+1}_2.png').convert()
+    list_temp.append(tempppp)
+lop6_baigiang.append(list_temp)
+
+list_temp = []
+for i in range(10):
+    tempppp = pygame.image.load(f'assets\\baigiang\\lop6\\bai{i+1}_3.png').convert()
+    list_temp.append(tempppp)
+lop6_baigiang.append(list_temp)
