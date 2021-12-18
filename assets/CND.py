dd4904375744c9d883c44f9850403e9410c76edd
HN <nbminerga@gmail.com>
2021-12-18 21:18:35 +0700

    OKK

--- assets/CND.py
+++ assets/CND.py
@@ -77,8 +77,39 @@ def show_label(scr,text,font,color,center_pos):
 
 
 # X O Games ============================================== #
+XO_selected = None
+def set_selected(which):
+	global XO_selected
+	XO_selected = which
 XO_running = True
 XO_wait = True
 XO_O = ['n','n','n','n','n','n','n','n','n']
 XO_turn = 'p'
-XO_bg = pygame.image.load('')
+XO_bg = pygame.image.load('assets\\backgrounds\\X_O_bg.png').convert_alpha()
+XO_buttons = []
+temp1 = pygame.image.load('assets\\buttons\\X_O_button.png').convert()
+temp2 = pygame.image.load('assets\\buttons\\X_O_button_1.png').convert()
+for i in range(9):
+	if i == 0:
+		temp = N_Button(temp1,temp2,(116,103),lambda: set_selected(0))
+	elif i == 1:
+		temp = N_Button(temp1,temp2,(116,103),lambda: set_selected(1))
+	elif i == 2:
+		temp = N_Button(temp1,temp2,(116,103),lambda: set_selected(2))
+	elif i == 3:
+		temp = N_Button(temp1,temp2,(116,103),lambda: set_selected(3))
+	elif i == 4:
+		temp = N_Button(temp1,temp2,(116,103),lambda: set_selected(4))
+	elif i == 5:
+		temp = N_Button(temp1,temp2,(116,103),lambda: set_selected(5))
+	elif i == 6:
+		temp = N_Button(temp1,temp2,(116,103),lambda: set_selected(6))
+	elif i == 7:
+		temp = N_Button(temp1,temp2,(116,103),lambda: set_selected(7))
+	elif i == 8:
+		temp = N_Button(temp1,temp2,(116,103),lambda: set_selected(8))
+
+
+
+
+
