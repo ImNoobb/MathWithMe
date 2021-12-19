eb2cbc1cda6e346991dc15ba3325a642960a3aef
HN <nbminerga@gmail.com>
2021-12-19 15:46:57 +0700

    Complete the whole game

--- main.py
+++ main.py
@@ -353,7 +353,7 @@ setting_list = [setting_1,setting_2,setting_3,setting_4,setting_5,setting_6]
 setting_button = A_Button(setting_list,0.5,(1280-205,500),settings_cmd)
 # Back to lop 6 =================================================================== #
 def back_6():
-	global lop6,lop6_Dai_I,lop6_Dai_II,lop6_Dai_III,lop6_Hinh_I,lop6_Hinh_II
+	global lop6,lop6_Dai_I,lop6_Dai_II,lop6_Dai_III,lop6_Hinh_I,lop6_Hinh_II,XO_l6
 	lop6_Dai_I = False
 	lop6_Dai_II = False
 	lop6_Dai_III = False
@@ -716,6 +716,53 @@ def replay():
 	global XO_replay
 	XO_replay = True
 XO_replay_button = N_Button(temp1,temp2,(566,580),replay)
+def back_XO():
+	global XO_l6,XO_selected,XO_running,XO_wait,XO_O,XO_turn,XO_x1,XO_x2,XO_ans,XO_q_ans,XO_question,XO_win,XO_lose,XO_index,XO_RAP,XO_AnsButton,XO_a,XO_a_pos,XO_OK,XO_pos,XO_replay,XO_text
+	XO_l6 = False
+	XO_font = pygame.font.Font(None,25)
+	XO_selected = None
+	XO_wait = False
+	XO_running = True
+	XO_wait = True
+	XO_O = ['n','n','n','n','n','n','n','n','n']
+	XO_turn = 'p'
+	XO_bg = pygame.image.load('assets\\backgrounds\\X_O_bg.png').convert()
+	temp1 = pygame.image.load('assets\\buttons\\X_O_button.png').convert()
+	temp2 = pygame.image.load('assets\\buttons\\X_O_button_1.png').convert()
+	XO_button = [temp1,temp2]
+	XO_x1,XO_x2 = 0,0
+	XO_ans = 0
+	XO_q_ans = 0
+	XO_question = ''
+	XO_Ximg = pygame.image.load('assets\\buttons\\X_button_.png').convert()
+	XO_Oimg = pygame.image.load('assets\\buttons\\O_button.png').convert()
+	XO_win = False
+	XO_lose = False
+	XO_rect = [temp1.get_rect(topleft=(116,103)),temp1.get_rect(topleft=(243,103)),temp1.get_rect(topleft=(370,103)),temp1.get_rect(topleft=(116,229)),temp1.get_rect(topleft=(243,229)),temp1.get_rect(topleft=(370,229)),temp1.get_rect(topleft=(116,355)),temp1.get_rect(topleft=(243,355)),temp1.get_rect(topleft=(370,355))]
+	XO_index = [0,0,0,0,0,0,0,0,0]
+	XO_RAP = None
+	XO_AnsButton = []
+	temp1 = pygame.image.load('assets\\buttons\\ans_1.png').convert_alpha()
+	temp2 = pygame.image.load('assets\\buttons\\ans_2.png').convert_alpha()
+	for i in range(4):
+		if i == 0:
+			temp = T_Button(temp1,temp2,(41,621),i,XO_font)
+		elif i == 1:
+			temp = T_Button(temp1,temp2,(241,621),i,XO_font)
+		elif i == 2:
+			temp = T_Button(temp1,temp2,(441,621),i,XO_font)
+		elif i == 3:
+			temp = T_Button(temp1,temp2,(641,621),i,XO_font)
+		XO_AnsButton.append(temp)
+
+	XO_a = [None,None,None]
+	XO_a_pos = [None,None,None]
+	XO_OK = False
+	XO_player_choose_img = pygame.image.load('assets\\buttons\\XO_player_choose.png').convert()
+	XO_pos = None
+	XO_replay = False
+	XO_text = ['Đang là lượt của bạn, hãy chọn một ô']
+XO_back_button = N_Button(back1,back2,(0,0),back_XO)
 # BackGrounds ===================================================================== #
 lop6_bg = pygame.image.load('assets\\backgrounds\\lop6_bg.png').convert()
 lop7_bg = pygame.image.load('assets\\backgrounds\\lop7_bg.png').convert()
@@ -1311,6 +1358,7 @@ while running:
 					list_temp.append(temp)
 				XO_text = list_temp
 			show_label_tl(screen,XO_text,font,(740,147))
+			XO_back_button.run(screen,click,s_m_o_f)
 
 		else:
 			screen.blit(lop6_bg,(0,0))
