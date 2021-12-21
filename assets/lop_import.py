7f850b3b1ff5485224cde753f91a20abf4d5f5bf
HN <nbminerga@gmail.com>
2021-12-22 06:44:58 +0700

    dad

--- assets/lop_import.py
+++ assets/lop_import.py
@@ -90,3 +90,18 @@ for i in range(9):
     tempppp = pygame.image.load(f'assets\\baigiang\\lop8\\bai{i+1}.png').convert()
     list_temp.append(tempppp)
 lop8_baigiang.append(list_temp)
+
+lop8_baigiang.append(['n'])
+lop8_baigiang.append(['n'])
+
+list_temp = []
+for i in range(12):
+    tempppp = pygame.image.load(f'assets\\baigiang\\lop8\\bai{i+1}_4.png').convert()
+    list_temp.append(tempppp)
+lop8_baigiang.append(list_temp)
+
+list_temp = []
+for i in range(6):
+    tempppp = pygame.image.load(f'assets\\baigiang\\lop8\\bai{i+1}_5.png').convert()
+    list_temp.append(tempppp)
+lop8_baigiang.append(list_temp)
